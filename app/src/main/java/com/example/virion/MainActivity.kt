e260b7c383007476108910335acb0784419effd7
Paola <46930649+paolaparadas29@users.noreply.github.com>
2021-07-11 21:33:51 +0200

    First commit

--- app/src/main/java/com/example/virion/MainActivity.kt
+++ app/src/main/java/com/example/virion/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.virion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.zxing.integration.android.IntentIntegrator
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val scan_button = findViewById<Button>(R.id.scan_button)
+        scan_button.setOnClickListener {
+            val scanner = IntentIntegrator(this)
+
+            scanner.initiateScan()
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        val result = IntentIntegrator.parseActivityResult(requestCode, resultCode, data)
+        if (result != null) {
+            if (result.contents == null) {
+                Toast.makeText(this, "Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(this, "Scanned: " + result.contents, Toast.LENGTH_LONG).show()
+            }
+        } else {
+            super.onActivityResult(requestCode, resultCode, data)
+        }
     }
 }
